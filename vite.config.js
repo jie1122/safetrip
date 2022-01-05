4ed5255c273b72713efd9c5a6bf6d6a57699df9c
jie112 <2223889679@qq.com>
2022-01-06 04:10:45 +0800

    5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'./',
+  build:{
+    outDir:'docs'
+  },
   css: {
     postcss: {
       plugins: [
