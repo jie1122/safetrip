292ca2822f1bc10a7acc00f756039c3023b56eb8
jie1122 <44047656+jie1122@users.noreply.github.com>
2024-11-05 17:08:30 +0800

    去掉异步路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import { createRouter,createWebHashHistory  } from 'vue-router'
+import home from '../views/home.vue'
+import safeTripForm from '../views/safeTripForm.vue'
+import safeTrip from '../views/safeTrip.vue'
+import examResult from '../views/examResult.vue'
+import examForm from '../views/examForm.vue'
+import siteCodeForm from '../views/siteCodeForm.vue'
+import siteCode from '../views/siteCode.vue'
+
+
 
 const routes = [
   
-  { path: '/', component: () => import('../views/home.vue') },
-  { path: '/safeTripForm', component: () => import('../views/safeTripForm.vue')},
-  { path: '/safeTrip', component: () => import('../views/safeTrip.vue')  },
-  { path: '/examResult', component: () => import('../views/examResult.vue')},
-  { path: '/examForm', component: () => import('../views/examForm.vue') },
-  { path: '/siteCodeForm', component: () => import('../views/siteCodeForm.vue') },
-  { path: '/siteCode', component: () => import('../views/siteCode.vue') },
+  { path: '/', component: home },
+  { path: '/safeTripForm', component: safeTripForm },
+  { path: '/safeTrip', component: safeTrip  },
+  { path: '/examResult', component: examResult },
+  { path: '/examForm', component: examForm },
+  { path: '/siteCodeForm', component: siteCodeForm },
+  { path: '/siteCode', component: siteCode },
 ]
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
